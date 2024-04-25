7cc11a6a280ab72dbee63937c4fefa2235938ddf
aaronnicholashaigh <103605576+aaronnicholashaigh@users.noreply.github.com>
2024-04-25 17:09:49 -0500

    Updated Hero.tsx
    added my intro to the hero and added a gradient image

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import BlurArrow from '../../public/assets/blue-button.svg'
+import Gradient from '../../public/assets/Gradient.svg'
 
 export function Hero(){
     return(
         <div className="pt-4">
             <div className="px-[20px]">
-                <h1 className="text-center text-[32px] leading-[40px] font-medium text-[#172026]">This is the Hero component.  I have not decided what is going here???</h1>
+                <h1 className="text-center text-[32px] leading-[40px] font-medium text-[#172026]">Welcome to my, extremly, work in progress site.  This page is created with the help of Vercel and I use it to practice.  Essentially, this project
+        provides me more experience with website design, general coding, and QA automation.  Stay tuned!</h1>
                 <p className="text-center pt-6">In web design, a hero is a large image, video, illustration, or banner that appears at the top of a page, usually above the fold and below the header. Heroes are also known as "hero headers" or "hero sections". They are important because they can:
                 <br />-Grab the users attention
                 <br />-Provide a quick visual message about the pages content
@@ -25,6 +27,10 @@ export function Hero(){
                     </span>
                     </button>
                 </div>
+            </div>
+
+            <div className="relative flex h-full w-full justify-center">
+                <Image src={Gradient} alt="Gradient" className="min-h-[500px]"/>
             </div>
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,6 @@ export default function Home() {
     <div>
       <Navbar />
       <Hero />
-    
-      <h1 className="flex w-full items-center justify-between px-[20px] py-[16px] lg:container lg:mx-auto lg:px-20">
-        Welcome to my, extremly, work in progress site.  This page is created with the help of Vercel and I use it to practice.  Essentially, this project
-        provides me more experience with website design, general coding, and QA automation.  Stay tuned!
-      </h1>
     </div>
     
   );
