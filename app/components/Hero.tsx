4e20a238ae2c14772fc216a5737dce1dc6406c2d
aaronnicholashaigh <103605576+aaronnicholashaigh@users.noreply.github.com>
2024-04-25 16:06:53 -0500

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,8 +4,8 @@ export function Hero(){
             <div className="px-[20px] text-[32px] leading-[40px] font-medium text-[#172026]">
                 <h1 className="text-center ">This is the Hero component.  I have not decided what is going here???</h1>
                 <p className="text-center pt-6">In web design, a hero is a large image, video, illustration, or banner that appears at the top of a page, usually above the fold and below the header. Heroes are also known as "hero headers" or "hero sections". They are important because they can:
-                <br />-Grab the user's attention
-                <br />-Provide a quick visual message about the page's content
+                <br />-Grab the users attention
+                <br />-Provide a quick visual message about the pages content
                 <br />-Set the emotional background of the user experience
                 <br />-Help the user navigate to the right information
                 </p>
