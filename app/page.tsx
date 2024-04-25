2e30c1bbdf46cbe2d9d9ce973c7ef841fedac618
aaronnicholashaigh <103605576+aaronnicholashaigh@users.noreply.github.com>
2024-04-25 09:55:40 -0500

    NavBar work
    Update for NavBar progress.  Added header links (text and design at least).

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,8 @@ export default function Home() {
     <div>
       <Navbar />
     
-      <h1>Welcome to my, extremly, work in progress site.  This page is created with the help of Vercel and I use it to practice.  Essentially, this project
+      <h1 className="flex w-full items-center justify-between px-[20px] py-[16px] lg:container lg:mx-auto lg:px-20">
+        Welcome to my, extremly, work in progress site.  This page is created with the help of Vercel and I use it to practice.  Essentially, this project
         provides me more experience with website design, general coding, and QA automation.  Stay tuned!
       </h1>
     </div>
