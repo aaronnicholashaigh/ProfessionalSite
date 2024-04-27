de89c78353da02a10f129932fcc93d76e9e413ec
aaronnicholashaigh <103605576+aaronnicholashaigh@users.noreply.github.com>
2024-04-27 09:49:38 -0500

    Working Header Link
    used a new header to create working header links.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Navbar } from "./components/Navbar";
+import { NewNavbar } from "./components/NewNavbar";
 import { Hero } from "./components/Hero";
+
+/*
+Tutorials used for this project:
+https://www.youtube.com/watch?v=pQ7tm_7S_Us
+https://www.youtube.com/watch?v=W09K95djOy0
+*/
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
+      <NewNavbar />
       <Hero />
     </div>
     
