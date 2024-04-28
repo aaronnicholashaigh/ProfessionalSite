5ecb6ca2a6575fae2efbceb777cbac4ca7f5de04
aaronnicholashaigh <103605576+aaronnicholashaigh@users.noreply.github.com>
2024-04-28 17:14:47 -0500

    Update page.tsx
    again

--- app/hobbies/page.tsx
+++ app/hobbies/page.tsx
@@ -2,11 +2,12 @@ import Head from "next/head";
 
 export default function Page(){
     
-    return 
-    <div>
+    return (
+        <div>
         <Head>
         <title>Aaron Haigh's Professional Site</title>
       </Head>
       <h1>This is the Hobbies page</h1>
       </div>
+    )
   }
